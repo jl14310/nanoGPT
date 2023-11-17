7de406bee02969f5b2fdc39414f054f49e4404b9
jl14310 <123594688+jl14310@users.noreply.github.com>
2023-11-17 02:05:16 -0500

    Update configurator.py

--- configurator.py
+++ configurator.py
@@ -18,7 +18,9 @@ import sys
 from ast import literal_eval
 
 for arg in sys.argv[1:]:
+    print(arg)
     if '=' not in arg:
+        print('not in',arg)
         # assume it's the name of a config file
         assert not arg.startswith('--')
         config_file = arg
@@ -27,6 +29,7 @@ for arg in sys.argv[1:]:
             print(f.read())
         exec(open(config_file).read())
     else:
+        print('in',arg)
         # assume it's a --key=value argument
         assert arg.startswith('--')
         key, val = arg.split('=')
