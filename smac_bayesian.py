4906e5ea70639c44a6d3e9156dd1198eb41920b2
jl14310 <123594688+jl14310@users.noreply.github.com>
2023-11-27 18:38:21 -0500

    Update smac_bayesian.py

--- smac_bayesian.py
+++ smac_bayesian.py
@@ -237,7 +237,7 @@ if __name__ == "__main__":
         # Initial setup if no saved state exists
         print('initialized')
         model = gpt2(seed, modeltype)
-        scenario = Scenario(model.configspace, deterministic=False, output_directory=f'state_files/seed_{seed}', n_trials=100, seed=seed)
+        scenario = Scenario(model.configspace, deterministic=False, output_directory=f'state_files/{modeltype}_seed_{seed}', n_trials=100, seed=seed)
         initial = RandomInitialDesign(scenario, n_configs=2)
         intensifier = HyperparameterOptimizationFacade.get_intensifier(scenario, max_config_calls=1)
         smac = HyperparameterOptimizationFacade(scenario, model.train, intensifier=intensifier, initial_design=initial, overwrite=True)
