wrong results

model.py
@@ -101,8 +101,8 @@ class Block(nn.Module):
         self.mlp = MLP(config)
 
     def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.mlp(self.ln_2(x))
+        x = x #+ self.attn(self.ln_1(x))
+        x = x #+ self.mlp(self.ln_2(x))
         return x
 
 @dataclass
@@ -128,11 +128,10 @@ class GPT(nn.Module):
             wpe = nn.Embedding(config.block_size, config.n_embd),
             drop = nn.Dropout(config.dropout),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = LayerNorm(config.n_embd, bias=config.bias), # layer norm setting
         ))
-       
-            
-         
+        """
+            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+         """
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         # with weight tying when using torch.compile() some warnings get generated:
         # "UserWarning: functional_call was passed multiple values for tied weights.
@@ -182,7 +181,7 @@ class GPT(nn.Module):
         
         for block in self.transformer.h:
             x = block(x)
-        x = self.transformer.ln_f(x) # comment if layer norm yes
+        #x = self.transformer.ln_f(x)
         
         if targets is not None:
             # if we are given some desired targets also calculate the loss